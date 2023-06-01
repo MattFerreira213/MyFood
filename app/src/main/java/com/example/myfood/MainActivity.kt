e1eff3372359227121a60830cecd4d1ae1eed1e1
Mateus Ferreira <mateusanjos225@gmail.com>
2023-05-31 23:20:47 -0300

    Construção da parte logica + comunicação com interface grafica

--- app/src/main/java/com/example/myfood/MainActivity.kt
+++ app/src/main/java/com/example/myfood/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.myfood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TableLayout
+import com.example.myfood.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        steupViews()
+    }
+
+    private fun steupViews(){
+        val tableLayout = binding.addTab
+        val viewPage = binding.addViewpager
+
+        TabLayoutMediator(tableLayout, viewPage) {
+            tab, position -> tab.text = "Novo texto"
+        }.attach()
     }
 }
